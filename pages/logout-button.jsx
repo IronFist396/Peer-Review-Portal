62ec313482f1e63338cacd666c2cf878929e1e21
IronFist396 <atharvroday396@gmail.com>
2026-02-28 21:00:11 +0530

    fixing logout bug

--- pages/logout-button.jsx
+++ pages/logout-button.jsx
@@ -4,7 +4,7 @@ import { signOut } from "next-auth/react";
 export default function LogoutButton() {
   return (
     <button
-      onClick={() => signOut({ callbackUrl: "/home" })}
+      onClick={() => signOut({ callbackUrl: `${window.location.origin}/home` })}
       className="px-4 py-2 text-white bg-red-500 rounded hover:bg-red-600"
     >
       Sign Out
